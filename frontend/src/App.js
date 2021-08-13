2edafb3c78b630934d7561900638b0e9ceeb0c1c
zcurranz <48265548+zcurranz@users.noreply.github.com>
2021-08-13 22:11:10 +1200

    added 404 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import {
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article"; 
-//import NotFoundPage from "./pages/404";
+import NotFoundPage from "./pages/404";
 
 const App = () =>  {
     return (
